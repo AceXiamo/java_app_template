661329dd94b403a022a20b23830ece63a914a514
AceXiamo <xiamo@qwq.link>
2024-05-27 02:50:54 +0800

    feat: Add types for main data structures

---
+++ src/types/main.d.ts
@@ -0,0 +1,45 @@
+type ShowMode = 'view' | 'edit'
+
+interface LoginReqData {
+  code: string
+}
+
+type UploadStatus = 'wait' | 'run' | 'finish'
+
+interface ResponseData<T> {
+  code: number
+  msg: string
+  data: T
+}
+
+interface ResponsePageData<T> {
+  code: number
+  msg: string
+  data: PageData<T>
+}
+
+interface PageData<T> {
+  countId: any
+  current: number
+  maxLimit: number
+  optimizeCountSql: boolean
+  orders: any[]
+  pages: number
+  records: T[]
+  searchCount: boolean
+  size: number
+  total: number
+}
+
+interface User {
+  openId?: string
+  nickname?: string
+  avatar?: string
+  isBan?: string
+  createTime?: string
+  updateTime?: string
+  banEndTime?: string
+  descOfBan?: string
+  token?: string
+  sessionKey?: string
+}
