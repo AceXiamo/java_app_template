a8f0af6b9fff9da389668225ae25569e9ceafc1a
AceXiamo <xiamo@qwq.link>
2024-05-27 02:50:34 +0800

    feat: add component `HeadBar`

---
+++ src/components/HeadBar.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { useSettingStore } from '@/store/setting'
+
+withDefaults(defineProps<{
+  bgColor: string
+}>(), {
+  bgColor: 'white',
+})
+
+const setting = useSettingStore()
+</script>
+
+<template>
+  <view class="relative flex flex-col pb-20rpx" :style="{ backgroundColor: $props.bgColor }">
+    <view :style="{ height: `${setting.topH}rpx` }" />
+    <view :style="{ height: `${setting.capsuleH}rpx` }">
+      <slot />
+    </view>
+  </view>
+</template>
