cdbac476b1c2ea0493e86f4ce8361997fdcb2164
AceXiamo <xiamo@qwq.link>
2024-05-27 03:01:05 +0800

    feat: update setting store on app load

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <script setup lang="ts">
-onLaunch(() => {})
+import { useSettingStore } from '@/store/setting'
+
+const setting = useSettingStore()
+onLaunch(async () => {
+  const res = uni.getMenuButtonBoundingClientRect()
+  // to rpx
+  setting.setTopH(res.top * 2)
+  setting.setCapsuleH(res.height * 2)
+
+  const info: any = await uni.getSystemInfo()
+  let isAppleAndHasLine = false
+  if (info.model?.toLowerCase().includes('ip')) {
+    const regex = /\d+/g
+    const matches = info.model.match(regex)
+    if (matches && matches.length > 0)
+      isAppleAndHasLine = matches[0] > 8
+    else
+      isAppleAndHasLine = info.model.toLowerCase().includes('x')
+  }
+  setting.setIsAppleAndHasLine(isAppleAndHasLine)
+})
 </script>
