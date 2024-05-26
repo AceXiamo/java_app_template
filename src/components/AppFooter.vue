21849081ad24c9a917e07b571638e0dea74ccff1
AceXiamo <xiamo@qwq.link>
2024-05-27 02:48:17 +0800

    feat: main tab bar

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,25 +1,73 @@
 <script lang="ts" setup>
-function handleClickGithub() {
-  if (window?.open) {
-    window.open('https://github.com/uni-helper/vitesse-uni-app')
-  }
-  else {
-    uni.showToast({
-      icon: 'none',
-      title: '请使用浏览器打开',
-    })
+import { useSettingStore } from '@/store/setting'
+import type { ActiveMenu } from '@/store/menu'
+import { useMenuStore } from '@/store/menu'
+
+uni.hideTabBar()
+const setting = useSettingStore()
+const { active } = toRefs(useMenuStore())
+
+function menuClick(key: ActiveMenu) {
+  active.value = key
+  switch (key) {
+    case 'home':
+      uni.switchTab({
+        url: '/pages/main/index',
+      })
+      break
+    case 'exam':
+      uni.switchTab({
+        url: '/pages/exam/index',
+      })
+      break
+    case 'my':
+      uni.switchTab({
+        url: '/pages/my/index',
+      })
+      break
   }
 }
 </script>
 
 <template>
-  <view text="xl gray4" m-5 flex items-center justify-center gap-3>
-    <navigator url="/pages/index" open-type="redirect">
-      <view i-carbon-campsite />
-    </navigator>
-
-    <view cursor-pointer @click="handleClickGithub">
-      <view i-carbon:logo-github />
+  <view
+    :style="{
+      paddingBottom: setting.isAppleAndHasLine ? 'calc(env(safe-area-inset-bottom) + 10rpx)' : '20rpx',
+    }"
+    flex justify-between bg-white px-[100rpx] pt-[20rpx]
+  >
+    <view flex flex-col items-center gap-[10rpx] @tap="menuClick('home')">
+      <template v-if="active === 'home'">
+        <view i-heroicons:home-solid text-[40rpx] text-[#7ab6e0] />
+      </template>
+      <template v-else>
+        <view i-heroicons:home text-[40rpx] text-[#999] />
+      </template>
+      <text text-[24rpx]>
+        首页
+      </text>
+    </view>
+    <view flex flex-col items-center gap-[10rpx] @tap="menuClick('exam')">
+      <template v-if="active === 'exam'">
+        <view i-heroicons:trophy-solid text-[40rpx] text-[#7ab6e0] />
+      </template>
+      <template v-else>
+        <view i-heroicons:trophy text-[40rpx] text-[#999] />
+      </template>
+      <text text-[24rpx]>
+        举办比赛
+      </text>
+    </view>
+    <view flex flex-col items-center gap-[10rpx] @tap="menuClick('my')">
+      <template v-if="active === 'my'">
+        <view i-heroicons:star-solid text-[40rpx] text-[#7ab6e0] />
+      </template>
+      <template v-else>
+        <view i-heroicons:star text-[40rpx] text-[#999] />
+      </template>
+      <text text-[24rpx]>
+        我的
+      </text>
     </view>
   </view>
 </template>
