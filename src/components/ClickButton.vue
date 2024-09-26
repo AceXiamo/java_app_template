28e65ccd572a7278fbba58c146665386f6357540
AceXiamo <xiamo@qwq.link>
2024-09-27 01:34:53 +0800

    feat: add component `ClickButton`

---
+++ src/components/ClickButton.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+defineProps<{
+  type: 'primary' | 'warning' | 'danger' | 'default'
+  size: 'small' | 'medium' | 'large'
+}>()
+
+const emit = defineEmits(['click'])
+
+function handleClick() {
+  emit('click')
+}
+</script>
+
+<template>
+  <view
+    active:opacity="60"
+
+    box-border flex items-center
+    :style="{
+      backgroundColor: type === 'primary' ? '#1989fa' : type === 'warning' ? '#ff9760' : type === 'danger' ? '#f00' : 'transparent',
+      border: type === 'default' ? '1px solid #6b7280' : 'none',
+      padding: size === 'small' ? '8rpx 12rpx' : size === 'medium' ? '10rpx 16rpx' : '12rpx 20rpx',
+      borderRadius: size === 'small' ? '4rpx' : size === 'medium' ? '6rpx' : '8rpx',
+      fontSize: size === 'small' ? '18rpx' : size === 'medium' ? '20rpx' : '22rpx',
+      color: type === 'default' ? '#6b7280' : '#fff',
+    }"
+    @click="handleClick"
+  >
+    <slot />
+  </view>
+</template>
