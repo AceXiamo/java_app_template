4392d4903f1594c494fc29ec4d6434c1dd549080
AceXiamo <xiamo@qwq.link>
2024-10-02 23:04:32 +0800

    feat: page for `visitRecord`

---
+++ src/components/visitRecord/Head.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+function back() {
+  uni.navigateBack()
+}
+</script>
+
+<template>
+  <HeadBar bg-color="white">
+    <view relative h-full flex items-center justify-center>
+      <view i-heroicons:chevron-left-20-solid absolute left-[20rpx] text-[40rpx] text-black @click="back" />
+      <text text-[30rpx] text-black>
+        拜访记录
+      </text>
+    </view>
+  </HeadBar>
+</template>
---
+++ src/pages/my/visitRecord.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+
+<template>
+  <view h-full flex flex-col>
+    <VisitRecordHead />
+    <VisitRecordQuery mt-[20rpx] />
+    <view mt-[20rpx] h-0 flex-auto overflow-y-auto px-[20rpx] pb-[20rpx]>
+      <MainVisitItem />
+    </view>
+  </view>
+</template>
+
+<route lang="json">
+  {
+    "layout": "home"
+  }
+</route>
