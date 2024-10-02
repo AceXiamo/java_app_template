e9069eb6f1aee4a4e1c2798facf10bf4aeb2c9fa
AceXiamo <xiamo@qwq.link>
2024-10-02 23:00:34 +0800

    feat: page for `customerEdit`

---
+++ src/components/customerEdit/Head.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const title = ref('新增客户')
+
+onLoad((query: any) => {
+  const id = query.id
+  title.value = id ? '编辑客户' : '新增客户'
+})
+
+function back() {
+  uni.navigateBack()
+}
+</script>
+
+<template>
+  <HeadBar bg-color="white">
+    <view relative h-full flex items-center justify-center>
+      <view i-heroicons:chevron-left-20-solid absolute left-[20rpx] text-[40rpx] text-black @click="back" />
+      <text text-[30rpx] text-black>
+        {{ title }}
+      </text>
+    </view>
+  </HeadBar>
+</template>
---
+++ src/pages/customer/edit.vue
@@ -0,0 +1,180 @@
+<script lang="ts" setup>
+interface MapResult {
+  adcode: string
+  address: string
+  city: string[]
+  district: string
+  id: string
+  location: string
+  name: string
+  typecode: string
+}
+
+const markers = ref<Marker[]>([])
+const mapSearchVal = ref('')
+const showMapSearch = ref(false)
+const searchWithDebounce = debounce(handleMapSearch, 1000)
+const mapResult = ref<MapResult[]>([])
+const latitude = ref(0)
+const longitude = ref(0)
+
+function handleMapSearch() {
+  if (mapSearchVal.value) {
+    uni.request({
+      url: `https://restapi.amap.com/v3/assistant/inputtips?key=71e38cd17bc480d841eb195044145eb0&keywords=${mapSearchVal.value}`,
+      success: (res: any) => {
+        console.log(res)
+        showMapSearch.value = true
+        mapResult.value = res.data.tips
+      },
+    })
+  }
+}
+
+function handleMapSearchResultClick(item: MapResult) {
+  mapSearchVal.value = item.name
+  showMapSearch.value = false
+  const location = item.location.split(',')
+  latitude.value = Number(location[1])
+  longitude.value = Number(location[0])
+  markers.value = [
+    {
+      id: 1,
+      longitude: Number(location[0]),
+      latitude: Number(location[1]),
+      iconPath: 'https://file.qwq.link/icon/miniapp/location.png',
+      width: 30,
+      height: 30,
+    },
+  ]
+}
+
+function getLocation() {
+  uni.getLocation({
+    type: 'wgs84',
+    success: (res) => {
+      latitude.value = res.latitude
+      longitude.value = res.longitude
+    },
+    fail: (err) => {
+      logger.error(err)
+    },
+  })
+}
+
+onMounted(() => {
+  getLocation()
+})
+
+function submit() {
+  console.log('submit')
+}
+
+function back() {
+  uni.navigateBack()
+}
+</script>
+
+<template>
+  <view h-full flex flex-col>
+    <CustomerEditHead />
+    <view mt-[20rpx] h-0 flex flex-auto flex-col gap-[20rpx] overflow-y-auto px-[20rpx] pb-[20rpx]>
+      <view flex flex-col gap-[30rpx] rounded-[10rpx] bg-white p-[20rpx]>
+        <view flex flex-col justify-center gap-[20rpx]>
+          <view ml-[20rpx]>
+            <view i-heroicons:user-16-solid text-[26rpx] text-emerald-500 />
+            <text text-[26rpx] text-[#333]>
+              客户姓名
+            </text>
+            <text text-[30rpx] text-red-500>
+              *
+            </text>
+          </view>
+          <view box-border w-full border border-gray-300 rounded-[10rpx] border-solid p-[15rpx]>
+            <input type="text" placeholder="请输入客户姓名" class="text-[26rpx]">
+          </view>
+        </view>
+        <view flex flex-col justify-center gap-[20rpx]>
+          <view ml-[20rpx]>
+            <view i-heroicons:phone-16-solid text-[26rpx] text-emerald-500 />
+            <text text-[26rpx] text-[#333]>
+              客户电话
+            </text>
+            <text text-[30rpx] text-red-500>
+              *
+            </text>
+          </view>
+          <view box-border w-full border border-gray-300 rounded-[10rpx] border-solid p-[15rpx]>
+            <input type="text" placeholder="请输入客户电话" class="text-[26rpx]">
+          </view>
+        </view>
+        <view flex flex-col justify-center gap-[20rpx]>
+          <view ml-[20rpx]>
+            <view i-carbon:location-filled text-[26rpx] text-emerald-500 />
+            <text text-[26rpx] text-[#333]>
+              客户地址
+            </text>
+          </view>
+          <view relative box-border w-full flex items-center border border-gray-300 rounded-[10rpx] border-solid p-[15rpx]>
+            <input
+              v-model="mapSearchVal" type="text" placeholder="键入文字以搜索" class="w-full text-[26rpx]" :style="{
+                pointerEvents: showMapSearch ? 'none' : 'auto',
+              }"
+              @input="searchWithDebounce"
+            >
+            <view
+              absolute right-[20rpx] transition-all duration-200 :style="{
+                opacity: showMapSearch ? 1 : 0,
+                pointerEvents: showMapSearch ? 'auto' : 'none',
+              }"
+            >
+              <ClickButton size="medium" type="primary" @click="showMapSearch = false">
+                收起
+              </ClickButton>
+            </view>
+            <view
+              :style="{
+                bottom: showMapSearch ? '-20rpx' : '0',
+                opacity: showMapSearch ? 1 : 0,
+                pointerEvents: showMapSearch ? 'auto' : 'none',
+              }"
+              absolute bottom-[-20rpx] left-0 right-0 z-1 box-border max-h-[300rpx] flex flex-col translate-y-full gap-[10rpx] overflow-y-auto border border-gray-300 rounded-[10rpx] border-solid bg-white p-[10rpx] transition-all duration-200
+            >
+              <view v-for="item in mapResult" :key="item.id" box-border w-full rounded-[10rpx] p-[5rpx] text-[26rpx] @click="handleMapSearchResultClick(item)">
+                {{ item.name }}
+              </view>
+            </view>
+          </view>
+          <view h-[500rpx] w-full>
+            <map class="h-full w-full" :latitude="latitude" :longitude="longitude" :markers="markers" />
+          </view>
+        </view>
+        <view flex flex-col justify-center gap-[20rpx]>
+          <view ml-[20rpx]>
+            <view i-carbon:location-hazard-filled text-[26rpx] text-emerald-500 />
+            <text text-[26rpx] text-[#333]>
+              地址备注
+            </text>
+          </view>
+          <view box-border w-full border border-gray-300 rounded-[10rpx] border-solid p-[15rpx]>
+            <input type="text" placeholder="请输入地址备注" class="text-[26rpx]">
+          </view>
+        </view>
+      </view>
+      <view flex justify-end gap-[20rpx]>
+        <ClickButton type="primary" drop-shadow-lg @click="submit">
+          提交
+        </ClickButton>
+        <ClickButton type="default" drop-shadow-lg @click="back">
+          返回
+        </ClickButton>
+      </view>
+    </view>
+  </view>
+</template>
+
+<route lang="json">
+  {
+    "layout": "home"
+  }
+</route>
