ba4eba15d7a7d561902d778a43476e71bd8dc808
AceXiamo <xiamo@qwq.link>
2024-10-02 23:07:07 +0800

    refactor: remove un use fun

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,43 +1,8 @@
 import { host } from './request'
-import { getToken, setOpenId, setSessionKey, setToken } from './user'
-import logger from './logger'
-import { login } from '@/api/user'
-
-function saveLoginRes(res: User) {
-  setOpenId(res.openId!)
-  logger.setting('user', `openId: ${res.openId}`)
-  setSessionKey(res.sessionKey!)
-  logger.setting('user', `sessionKey: ${res.sessionKey}`)
-  setToken(res.token!)
-  logger.setting('user', `token: ${res.token}`)
-}
-
-function loginVerify() {
-  return !!getToken()
-}
-
-function getLoginUser() {
-  return uni.getStorageSync('user')
-}
 
 function fileUrlReplace(path: string) {
   return `${host}/file/view${path}`
 }
-
-function loginHandle() {
-  return new Promise((resolve) => {
-    uni.login({
-      provider: 'weixin',
-      success: async (result) => {
-        login({ code: result.code }).then((res) => {
-          saveLoginRes(res.data)
-          resolve('')
-        })
-      },
-    })
-  })
-}
-
 function stringToColor(str: string) {
   let hash = 0
   for (let i = 0; i < str.length; i++)
@@ -47,4 +12,4 @@ function stringToColor(str: string) {
   return `#${'00000'.substring(0, 6 - c.length)}${c}`
 }
 
-export { saveLoginRes, loginVerify, getLoginUser, fileUrlReplace, loginHandle, stringToColor }
+export { fileUrlReplace, stringToColor }
