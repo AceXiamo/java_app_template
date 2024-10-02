3c6897a0eb5e5a629bb98d31d23e8715fc23d775
AceXiamo <xiamo@qwq.link>
2024-10-02 22:56:29 +0800

    feat: user login api

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,43 +1,24 @@
-import { host, request } from '@/utils/request'
-
-export interface DataMaLoginRes {
-  code: 'SUCCESS' | 'PWD_ERROR' | 'PHONE_BIND'
-  openId: string
-  sessionKey: string
-  token: string
-}
-
-export function codeHandle(params: { code: string }): Promise<BaseRes<DataMaLoginRes>> {
-  return request.post({
-    url: `${host}/data/ma/codeHandle`,
-    params,
-  })
-}
-
-export interface DataMaPhoneBindAndLoginParams {
-  openId: string
-  encryptedData?: string
-  iv?: string
+export interface LoginResult extends WxUser {
   sessionKey?: string
+  token?: string
 }
 
-export function loginByPhone(params: DataMaPhoneBindAndLoginParams, toast?: any): Promise<BaseRes<DataMaLoginRes>> {
+export interface WxUser {
+  openId: string
+  nickname: string
+  avatar: string
+  userId: number
+  delFlag: number
+  createTime: string
+  appId?: string
+}
+
+export function login(params: { code: string }): Promise<BaseRes<LoginResult>> {
   return request.post({
-    url: `${host}/data/ma/loginByPhone`,
-    params,
-    toast,
-  })
-}
-
-export interface PwdLoginParams {
-  username: string
-  password: string
-}
-
-export function loginByAccount(params: PwdLoginParams, toast?: any): Promise<BaseRes<DataMaLoginRes>> {
-  return request.post({
-    url: `${host}/data/ma/loginByAccount`,
-    params,
-    toast,
+    url: `${host}/wechat/login`,
+    params: {
+      ...params,
+      appId: 'wx30d7fd774ea643d1',
+    },
   })
 }
--- src/api/user.ts
+++ src/api/user.ts
@@ -1,8 +1,11 @@
 import { host, request } from '@/utils/request'
 
-export function login(params: LoginReqData): Promise<ResponseData<User>> {
+export function loginByPwd(data: {
+  username: string
+  password: string
+}): Promise<{ code: number, msg: string, token: string }> {
   return request.post({
-    url: `${host}/wx/login`,
-    params,
+    url: `${host}/anotherLogin`,
+    data,
   })
 }
