1354b8bc806eb6670710ba761226f18d17b418fc
AceXiamo <xiamo@qwq.link>
2024-10-02 22:58:49 +0800

    refactor: support custom color

--- src/components/ClickButton.vue
+++ src/components/ClickButton.vue
@@ -2,8 +2,10 @@
 import { defineProps } from 'vue'
 
 defineProps<{
-  type: 'primary' | 'warning' | 'danger' | 'default'
+  type: 'primary' | 'warning' | 'danger' | 'default' | 'success'
   size: 'small' | 'medium' | 'large'
+  bgColor?: string
+  fontColor?: string
 }>()
 
 const emit = defineEmits(['click'])
@@ -16,15 +18,14 @@ function handleClick() {
 <template>
   <view
     active:opacity="60"
-
     box-border flex items-center
     :style="{
-      backgroundColor: type === 'primary' ? '#1989fa' : type === 'warning' ? '#ff9760' : type === 'danger' ? '#f00' : 'transparent',
-      border: type === 'default' ? '1px solid #6b7280' : 'none',
+      backgroundColor: bgColor ?? (type === 'primary' ? '#1989fa' : type === 'warning' ? '#ff9760' : type === 'danger' ? '#ef4444' : type === 'success' ? '#16a34a' : 'transparent'),
+      border: type === 'default' ? '1px solid #6b7280' : '1px solid transparent',
       padding: size === 'small' ? '8rpx 12rpx' : size === 'medium' ? '10rpx 16rpx' : '12rpx 20rpx',
       borderRadius: size === 'small' ? '4rpx' : size === 'medium' ? '6rpx' : '8rpx',
       fontSize: size === 'small' ? '18rpx' : size === 'medium' ? '20rpx' : '22rpx',
-      color: type === 'default' ? '#6b7280' : '#fff',
+      color: fontColor ?? (type === 'default' ? '#6b7280' : '#fff'),
     }"
     @click="handleClick"
   >
