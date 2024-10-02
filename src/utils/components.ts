11e872842914a61c1fed2f9a8be943c4ae02daeb
AceXiamo <xiamo@qwq.link>
2024-10-02 23:06:52 +0800

    feat: cache global component `InputConfirmRef`

--- src/utils/components.ts
+++ src/utils/components.ts
@@ -1,6 +1,8 @@
 import { ref } from 'vue'
 import type Toast from '@/components/Toast.vue'
 import type Confirm from '@/components/Confirm.vue'
+import type InputConfirm from '@/components/InputConfirm.vue'
 
 export const toastRef = ref<InstanceType<typeof Toast>>()
 export const confirmRef = ref<InstanceType<typeof Confirm>>()
+export const inputConfirmRef = ref<InstanceType<typeof InputConfirm>>()
