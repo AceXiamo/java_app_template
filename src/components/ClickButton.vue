c15609409ba54fef092ed7c4e0b04ec96c0f2445
AceXiamo <xiamo@qwq.link>
2025-01-09 18:34:40 +0800

    feat: add text support

--- src/components/ClickButton.vue
+++ src/components/ClickButton.vue
@@ -2,8 +2,9 @@
 import { defineProps } from 'vue'
 
 defineProps<{
-  type: 'primary' | 'warning' | 'danger' | 'default' | 'success'
-  size: 'small' | 'medium' | 'large'
+  type?: 'primary' | 'warning' | 'danger' | 'default' | 'success'
+  text?: boolean
+  size?: 'small' | 'medium' | 'large'
   bgColor?: string
   fontColor?: string
   label?: string
@@ -21,12 +22,17 @@ function handleClick() {
     active:opacity="60"
     box-border flex items-center
     :style="{
-      backgroundColor: bgColor ?? (type === 'primary' ? '#1989fa' : type === 'warning' ? '#ff9760' : type === 'danger' ? '#ef4444' : type === 'success' ? '#16a34a' : 'transparent'),
+      ...(text ? {
+        backgroundColor: 'transparent',
+        color: type === 'primary' ? '#1989fa' : type === 'warning' ? '#ff9760' : type === 'danger' ? '#ef4444' : type === 'success' ? '#16a34a' : '#6b7280',
+      } : {
+        backgroundColor: bgColor ?? (type === 'primary' ? '#1989fa' : type === 'warning' ? '#ff9760' : type === 'danger' ? '#ef4444' : type === 'success' ? '#16a34a' : 'transparent'),
+        color: fontColor ?? (type === 'default' ? '#6b7280' : '#fff'),
+      }),
       border: type === 'default' ? '1px solid #6b7280' : '1px solid transparent',
       padding: size === 'small' ? '8rpx 12rpx' : size === 'medium' ? '10rpx 16rpx' : '12rpx 20rpx',
       borderRadius: size === 'small' ? '4rpx' : size === 'medium' ? '6rpx' : '8rpx',
       fontSize: size === 'small' ? '18rpx' : size === 'medium' ? '20rpx' : '22rpx',
-      color: fontColor ?? (type === 'default' ? '#6b7280' : '#fff'),
     }"
     @click="handleClick"
   >
