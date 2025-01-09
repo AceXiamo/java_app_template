5df7f637115a1734dade0491943fac47aa1da59d
AceXiamo <xiamo@qwq.link>
2025-01-09 18:39:40 +0800

    feat: api for dept

---
+++ src/api/dept.ts
@@ -0,0 +1,31 @@
+import type { DeptUserRef } from './deptUserRef'
+import { host, request } from '@/utils/request'
+
+export interface TenantDept {
+  id?: number
+  deptName?: string
+  tenantId?: number
+  updateTime?: Date
+  createTime?: Date
+  deptUserRefs?: DeptUserRef[]
+}
+
+export function getDeptList(params: PageQuery): Promise<BaseRes<PageData<TenantDept>>> {
+  return request.get({
+    url: `${host}/admin/tenantDept/list`,
+    params,
+  })
+}
+
+export function save(data: TenantDept): Promise<any> {
+  return request.post({
+    url: `${host}/admin/tenantDept/save`,
+    data,
+  })
+}
+
+export function del(params: { id: number }): Promise<any> {
+  return request.delete({
+    url: `${host}/admin/tenantDept/delete/${params.id}`,
+  })
+}
