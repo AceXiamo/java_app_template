555d154fbac48c350d7877cb389b56836cd29d73
AceXiamo <xiamo@qwq.link>
2025-01-11 06:45:46 +0800

    feat: add API functions for submitting visits and listing departments

---
+++ src/api/submitVisit.ts
@@ -0,0 +1,24 @@
+import type { CustomerVisit } from './customerVisit'
+import type { TenantDept } from './dept'
+import { host, request } from '@/utils/request'
+
+export function submitVisit(data: CustomerVisit) {
+  return request.post({
+    url: `${host}/app/customer/visit/commit`,
+    data,
+  })
+}
+
+export function listDept(params: { tenantId: number }): Promise<BaseRes<TenantDept[]>> {
+  return request.get({
+    url: `${host}/app/customer/visit/listDept`,
+    params,
+  })
+}
+
+export function listForCustomer(params: { openId: string }) {
+  return request.get({
+    url: `${host}/app/customer/visit/listForCustomer`,
+    params,
+  })
+}
